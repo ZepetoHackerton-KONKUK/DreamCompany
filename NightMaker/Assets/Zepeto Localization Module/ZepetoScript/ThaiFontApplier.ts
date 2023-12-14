87cb57e29849a3be392672af2aef753005cc5d33
JunKyu Choi <122720798+junkyn@users.noreply.github.com>
2023-12-14 11:47:28 +0900

    Delete NightMaker/Assets directory

--- NightMaker/Assets/Zepeto Localization Module/ZepetoScript/ThaiFontApplier.ts
+++
@@ -1,9 +0,0 @@
-import { Text } from "UnityEngine.UI";
-import { ZepetoScriptBehaviour } from "ZEPETO.Script";
-import Localization from "./Localization";
-
-export default class ThaiFontApplier extends ZepetoScriptBehaviour {
-    Start() {    
-        this.gameObject.GetComponent<Text>().font = Localization.instance.GetDefaultThaiFont();
-    }
-}
